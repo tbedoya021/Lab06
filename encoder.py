5690b68e0783040cf82c1b8ededdd16d5e9f6d02
Tanner Holland <holland.tanner@ufl.edu>
2023-10-25 14:34:33 -0400

    Tanner Holland finished decoder changes

--- encoder.py
+++ encoder.py
@@ -27,23 +27,35 @@ def encoder(password):
     return password_str
 
 
-
-
+def decoder(encoded_word):
+    encoded_list = [int(i) for i in encoded_word]
+    encoded_string = ''
+    for item in encoded_list:
+        if item in range(3, 10):
+            addition = item - 3
+        if item == 0:
+            addition = 7
+        elif item == 1:
+            addition = 8
+        elif item == 2:
+            addition = 9
+        encoded_string += str(addition)
+    return encoded_string
 
 
 def main():
     while True:
-        print("Menu \n------------- \n1. Encode \n2. Decode \n3. Quit")
+        print("Menu \n------------- \n1. Encode \n2. Decode \n3. Quit\n")
         selection = input("Please enter an option: ")
         if selection == '1':
             password = list(input("Please enter your password to encode: "))
             encoder_str = encoder(password)
             print("Your password has been encoded and stored! \n")
         elif selection == '2':
-            pass
+            encoded_password = decoder(encoder_str)
+            print(f"The encoded password is {encoder_str}, and the original password is {encoded_password}.\n")
         elif selection == '3':
             quit()
-
 
 
 if __name__ == "__main__":
